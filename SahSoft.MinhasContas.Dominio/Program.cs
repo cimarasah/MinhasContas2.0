d6c1fe7075b7b4cd30674f7fbbeaf37cbcac3951
Cimara Sá <cimarasah@gmail.com>
2020-03-12 00:40:08 -0300

    commit inicial - estrutura

---
+++ SahSoft.MinhasContas.Dominio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SahSoft.MinhasContas.Dominio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
